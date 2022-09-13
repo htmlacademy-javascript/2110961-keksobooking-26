149e2e75b7e7192a5be366fe7a58a6528e82cb9f
Alexey S <alexey787@ya.ru>
2022-09-14 05:53:08 +1000

    final v2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import './template.js';
+import './avatar.js';
+import './buildingphoto.js';
 import { advertsView} from './script.js';
 import { getData } from './api.js';
 import { setUserFormSubmit } from './form.js';
